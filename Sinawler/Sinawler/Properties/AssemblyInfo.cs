fbf06ec86d45eebedb51d0c2c9f9e6bc3a0b5f35
sizheng0320 <sizheng0320@5f0f3896-a7f3-8137-8510-6e69ecfdaa3e>
2010-10-11 19:50:35 +0000

    将调整频度时的减时增加为200毫秒

--- Sinawler/Sinawler/Properties/AssemblyInfo.cs
+++ Sinawler/Sinawler/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      内部版本号
 //      修订号
 //
-[assembly: AssemblyVersion( "1.2.6.1" )]
-[assembly: AssemblyFileVersion( "1.2.6.1" )]
+[assembly: AssemblyVersion( "1.2.6.2" )]
+[assembly: AssemblyFileVersion( "1.2.6.2" )]
 [assembly: NeutralResourcesLanguageAttribute("zh-CHS")]
