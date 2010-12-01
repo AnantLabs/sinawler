e3fb380dc4b45c3e193b9375a678f5de9d70032d
sizheng0320@gmail.com <sizheng0320@gmail.com@5f0f3896-a7f3-8137-8510-6e69ecfdaa3e>
2010-12-01 07:33:01 +0000

    去掉登录部分的无用代码

--- Sinawler/Sinawler/Properties/AssemblyInfo.cs
+++ Sinawler/Sinawler/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      内部版本号
 //      修订号
 //
-[assembly: AssemblyVersion( "5.1.0.0" )]
-[assembly: AssemblyFileVersion( "5.1.0.0" )]
+[assembly: AssemblyVersion( "5.1.0.1" )]
+[assembly: AssemblyFileVersion( "5.1.0.1" )]
 [assembly: NeutralResourcesLanguageAttribute("zh-CHS")]
